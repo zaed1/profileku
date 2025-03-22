c7b0f145e7439d8bdcff297e3f773d52ca667994
zaed1 <khmdzd@gmail.com>
2025-03-22 14:21:45 +0700

    cek

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/profileku/', // Set the base path for GitHub Pages
+  base: '/', // Set the base path for local development
   server: {
     host: "::",
     port: 8080,
