2a9c0227f862bc41bf1af23571d751e5cbdc4ae7
zaed1 <khmdzd@gmail.com>
2025-04-03 13:48:19 +0700

    predeploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { componentTagger } from "zaed-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/', // Set the base path for local development
+  base: '/profileku/', // Set the base path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
